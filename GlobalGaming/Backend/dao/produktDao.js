41bcd02e5c16d5546f1fd867bbb44a8573b4460f
Burak <burak.yildirtan@pixxie.de>
2022-12-06 22:09:45 +0100

    changed added

--- GlobalGaming/Backend/dao/produktDao.js
+++ GlobalGaming/Backend/dao/produktDao.js
@@ -12,83 +12,97 @@ class ProduktDao {
         return this._conn;
     }
 
-    loadById(id) {
-        // const produktkategorieDao = new ProduktkategorieDao(this._conn);
-        // const produktbildDao = new ProduktbildDao(this._conn);
+    create(titel, nettoPreis) {
 
-       
+        //Definieren von SQL Statement mit Values
+        var sql = 'INSERT INTO Produkt ( titel, nettoPreis) VALUES  (?,?)';
 
-        // if (helper.isUndefined(result)) 
-        //     throw new Error('No Record found by id=' + id);
-
-        // result.kategorie = produktkategorieDao.loadById(result.kategorieId);
-        // delete result.kategorieId;
-
-        // result.bilder = produktbildDao.loadByParent(result.id);
-
-
-        var sql = 'SELECT * FROM Produkt WHERE id=?';
+        //definieren von Statement zum ausführen als sql statement
         var statement = this._conn.prepare(sql);
-        var result = statement.get(id);
-        return result;
+
+
+        //Parameter der eingegebenen Produkt Details
+        var params = [titel, nettoPreis];
+
+        //ausführen von insert statement
+        var result = statement.run(params);
+
+
+        //wenn nicht eingeführt werden konnte
+        if (result.changes != 1) 
+            throw new Error('Dateien konnten nicht Eingefügt werden ' + params);
+
+        //zeigt das Produkt
+        return this.loadById(result.lastInsertRowid);
     }
 
-    loadAll() {
-        /*
-        const produktkategorieDao = new ProduktkategorieDao(this._conn);
-        const produktbildDao = new ProduktbildDao(this._conn);
+    delete(id) {
+        try {
+            //Anmerkung warscheinlich müssen bei einem Löschen von einem Produkt 
+            //zuerst die referenzierten IDs in anderen Tabellen gelöscht werden
+            //-------
+            // const produktbildDao = new ProduktbildDao(this._conn);
+            // produktbildDao.deleteByParent(id);
+            //-------
 
-       
+            var sql = 'DELETE FROM Produkt WHERE id=?';
+            var statement = this._conn.prepare(sql);
+            var result = statement.run(id);
 
-        if (helper.isArrayEmpty(result)) 
-            return [];
+            if (result.changes != 1) 
+                throw new Error('Produkt mit der ID =' + id+ ' konnte nicht gelöscht werden.');
 
-        for (var i = 0; i < result.length; i++) {
-            result[i].kategorie = produktkategorieDao.loadById(result[i].kategorieId);
-            delete result[i].kategorieid;
-
-
-            result[i].bilder = produktbildDao.loadByParent(result[i].id);
-
-
-        }
-        */
-        var sql = 'SELECT * FROM Produkt';
-        var statement = this._conn.prepare(sql);
-        var result = statement.all();
-        return result;
-    }
-
-    exists(id) {
-        var sql = 'SELECT COUNT(id) AS cnt FROM Produkt WHERE id=?';
-        var statement = this._conn.prepare(sql);
-        var result = statement.get(id);
-
-        if (result.cnt == 1) 
             return true;
-
-        return false;
+        } catch (ex) {
+            throw new Error('Could not delete Record by id=' + id + '. Reason: ' + ex.message);
+        }
     }
 
+    // loadById(id) {
+    //     // const produktkategorieDao = new ProduktkategorieDao(this._conn);
+    //     // const produktbildDao = new ProduktbildDao(this._conn);
 
-    // create(kategorieId = 1, bezeichnung = '', beschreibung = '',  details = null, nettopreis = 0.0,  bilder = []) {
+       
+
+    //     // if (helper.isUndefined(result)) 
+    //     //     throw new Error('No Record found by id=' + id);
+
+    //     // result.kategorie = produktkategorieDao.loadById(result.kategorieId);
+    //     // delete result.kategorieId;
+
+    //     // result.bilder = produktbildDao.loadByParent(result.id);
+
+
+    //     var sql = 'SELECT * FROM Produkt WHERE id=?';
+    //     var statement = this._conn.prepare(sql);
+    //     var result = statement.get(id);
+    //     return result;
+    // }
+
+    // loadAll() {
+    //     /*
+    //     const produktkategorieDao = new ProduktkategorieDao(this._conn);
     //     const produktbildDao = new ProduktbildDao(this._conn);
 
-    //     var sql = 'INSERT INTO Produkt (kategorieId,bezeichnung,beschreibung,details,nettopreis) VALUES (?,?,?,?,?,?,?)';
-    //     var statement = this._conn.prepare(sql);
-    //     var params = [kategorieId, bezeichnung, beschreibung, details, nettopreis];
-    //     var result = statement.run(params);
+       
 
-    //     if (result.changes != 1) 
-    //         throw new Error('Could not insert new Record. Data: ' + params);
+    //     if (helper.isArrayEmpty(result)) 
+    //         return [];
 
-    //     if (bilder.length > 0) {
-    //         for (var element of bilder) {
-    //             produktbildDao.create(element.bildpfad, result.lastInsertRowid);
-    //         }
+    //     for (var i = 0; i < result.length; i++) {
+    //         result[i].kategorie = produktkategorieDao.loadById(result[i].kategorieId);
+    //         delete result[i].kategorieid;
+
+
+    //         result[i].bilder = produktbildDao.loadByParent(result[i].id);
+
+
     //     }
-
-    //     return this.loadById(result.lastInsertRowid);
+    //     */
+    //     var sql = 'SELECT * FROM Produkt';
+    //     var statement = this._conn.prepare(sql);
+    //     var result = statement.all();
+    //     return result;
     // }
 
     // update(id, kategorieId = 1, bezeichnung = '', beschreibung = '', details = null, nettopreis = 0.0, bilder = []) {
@@ -110,24 +124,6 @@ class ProduktDao {
     //     }
 
     //     return this.loadById(id);
-    // }
-
-    // delete(id) {
-    //     try {
-    //         const produktbildDao = new ProduktbildDao(this._conn);
-    //         produktbildDao.deleteByParent(id);
-
-    //         var sql = 'DELETE FROM Produkt WHERE id=?';
-    //         var statement = this._conn.prepare(sql);
-    //         var result = statement.run(id);
-
-    //         if (result.changes != 1) 
-    //             throw new Error('Could not delete Record by id=' + id);
-
-    //         return true;
-    //     } catch (ex) {
-    //         throw new Error('Could not delete Record by id=' + id + '. Reason: ' + ex.message);
-    //     }
     // }
 
     toString() {
