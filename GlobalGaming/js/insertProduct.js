6785bfb4c0a98e16131390c386dc7bb09b47f720
burakyildirtan <burak.yildirtan@pixxie.de>
2022-12-06 19:16:50 +0100

    weiter Änderungen an js

--- GlobalGaming/js/insertProduct.js
+++ GlobalGaming/js/insertProduct.js
@@ -1,15 +1,57 @@
 
-//Dateiübergreifende Variablen
+//Buttons
+var rBSoftware = document.getElementById("inpSoftware");
+var rBHardware = document.getElementById("inpHardware");
+var checkBtnSale = document.getElementById("inpSale");
+var checkBtnCountdown = document.getElementById("inpCountdown");
+
+
+
+//Klassen
+var checkedBtn = document.getElementsByClassName("checkedCountdown")
+
+
+
+
+
+var cCheckBtnSale = document.getElementById("cCheckBtnSale");
+var cCheckBtnCountdown = document.getElementById("cCheckBtnCountdown");
+
+
 var cProductSpecs = document.getElementById("cProductSpecs");
 var cSoftwareSpecs = document.getElementById("cSoftwareSpecs");
-var rBSoftware = document.getElementById("inpSoftware");
+var cHardwareSpecs = document.getElementById("cHardwareSpecs");
 
+
+//Software Bereich
 rBSoftware.addEventListener ("click", () => {
     if(rBSoftware.checked) {
-        cProductSpecs.style.display = "block";
-        cSoftwareSpecs.style.display ="block";
-    } else {
-        cProductSpecs.style.display = "none";
+        cProductSpecs.style.display = "flex";
+        cSoftwareSpecs.style.display = "flex";
+        cHardwareSpecs.style.display = "flex";
     }
+});
 
+//Hardware Bereich 
+rBHardware.addEventListener ("click", () => {
+    if(rBHardware.checked) {
+        cProductSpecs.style.display = "flex";
+        cHardwareSpecs.style.display = "flex";
+    }
+});
+
+
+checkBtnSale.addEventListener ("click", () => {
+    if(checkBtnSale.checked) {
+
+    }
+});
+
+checkBtnCountdown.addEventListener ("click", () => {
+    if(checkBtnCountdown.checked) {
+        checkedBtn.style.display = "flex";
+    }else {
+        checkedBtn.style.display = "none";
+
+    }
 });
