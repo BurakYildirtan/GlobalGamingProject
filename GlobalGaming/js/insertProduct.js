53f56a651cac8fc4a64ed7186b6c9be2f837dc43
Burak <burak.yildirtan@pixxie.de>
2022-12-09 09:49:20 +0100

    last Commit before ABGABE amk

--- GlobalGaming/js/insertProduct.js
+++ GlobalGaming/js/insertProduct.js
@@ -14,6 +14,28 @@ var cSoftwareSpecs = document.getElementById("cSoftwareSpecs");
 var cHardwareSpecs = document.getElementById("cHardwareSpecs");
 var textBoxPicture = document.getElementById("productPicturePath");
 
+//input
+var inpPrice = document.getElementById("productPrice");
+var inpTitle = document.getElementById("productTitle");
+var inpPicturePath = document.getElementById("productPicturePath");
+var inpPlayer = document.getElementById("productPlayer");
+var inpGenre = document.getElementById("productGenre");
+var inpPerformance = document.getElementById("productPerformance");
+var inpReleaseDate = document.getElementById("productReleaseDate");
+var inpSalePerCent = document.getElementById("productSaleInPercent");
+var inpCountdownTime = document.getElementById("productCountdownTime");
+var inpCountdownSale = document.getElementById("productCountdownSale");
+
+//DatenSammlungen
+var productData;
+var softwareData;
+var hardwareData;
+var saleData;
+var countdownData;
+
+//response
+var spanResponse = document.querySelector("#response");
+
 //Software Bereich
 rBSoftware.addEventListener ("click", () => {
     if(rBSoftware.checked) {
@@ -21,7 +43,7 @@ rBSoftware.addEventListener ("click", () => {
         cSoftwareSpecs.style.display = "flex";
         cHardwareSpecs.style.display = "none";
         cCheckBtnSale.style.display = "flex";
-        btnSubmit.style.display = "block";
+        btnSubmit.style.display = "flex";
         
         
         document.getElementById("cSaleSpecs").style.display ="flex";
@@ -42,7 +64,7 @@ rBHardware.addEventListener ("click", () => {
         cHardwareSpecs.style.display = "flex";
         cSoftwareSpecs.style.display = "none";
         cCheckBtnSale.style.display = "flex";
-        btnSubmit.style.display = "block";
+        btnSubmit.style.display = "flex";
 
         document.getElementById("cSaleSpecs").style.display ="flex";
         checkBtnSale.checked = false;
@@ -54,7 +76,7 @@ rBHardware.addEventListener ("click", () => {
     }
 });
 
-//Sale Produkt 
+//Sale Bereich
 checkBtnSale.addEventListener ("click", () => {
     if(checkBtnSale.checked) {
         document.getElementById("cProductSaleInPercent").style.display="flex"
@@ -63,10 +85,13 @@ checkBtnSale.addEventListener ("click", () => {
     else{
         document.getElementById("cProductSaleInPercent").style.display="none"
         document.getElementById("cCheckBtnCountdown").style.display="none"
+        document.getElementById("cCountdownTime").style.display="none"
+        document.getElementById("cCountdownSaleInPercent").style.display="none"
+        checkBtnCountdown.checked = false
     }
 });
 
-//Countedown Produkt 
+//Countedown Bereich 
 checkBtnCountdown.addEventListener ("click", () => {
     if(checkBtnCountdown.checked) {
         document.getElementById("cCountdownTime").style.display="flex"
@@ -78,40 +103,135 @@ checkBtnCountdown.addEventListener ("click", () => {
     }
 });
 
-
+//Bild Preview
 textBoxPicture.addEventListener ("change", function() {
     document.getElementById("imgPreview").src = textBoxPicture.value;
 });
 
+//Preis überprüfen
+inpPrice.addEventListener("input", function() {
+    if (isNumeric(inpPrice.value)) {
+        inpPrice.style.border = "5px solid green";
+    }else{
+        inpPrice.style.border = "3px solid red";
+    }
+});
 
-$('#btnSubmit').click(async function() {
+//Titel überprüfen
+inpTitle.addEventListener("input", function() {
+    if (inpTitle.value != "" && inpTitle.value != undefined) {
+        inpTitle.style.border = "5px solid green";
+    }else{
+        inpTitle.style.border = "3px solid red";
+    }
+});
+
+//Bild überprüfen
+inpPicturePath.addEventListener("change", function() {
+    if (inpPicturePath.value != "" && inpPicturePath != undefined) {
+        inpPicturePath.style.border = "5px solid green";
+    }else{
+        inpPicturePath.style.border = "3px solid red";
+    }
+});
+
+//Spieleranzahl überprpüfen
+inpPlayer.addEventListener("input", function() {
+    if (inpPlayer.value <= 8 && inpPlayer.value >= 1) {
+        inpPlayer.style.border = "5px solid green";
+    }else{
+        inpPlayer.style.border = "3px solid red";
+    }
+});
+
+//Genre überprüfen
+inpGenre.addEventListener("input", function() {
+    if ( isAlphabetic(inpGenre.value) ) {
+        inpGenre.style.border = "5px solid green";
+    }else{
+        inpGenre.style.border = "3px solid red";
+    }
+});
+
+//Performance überprüfen
+inpPerformance.addEventListener("input", function() {
+    if(inpPerformance.value >= 1 && inpPerformance.value <= 100 ) {
+        inpPerformance.style.border = "5px solid green";
+    }else{
+        inpPerformance.style.border = "3px solid red";
+    }
+});
+
+//Erscheinungsdatum überprüfen
+inpReleaseDate.addEventListener("input", function() {
+    if(inpCountdownSale.value != undefined ) {
+        inpReleaseDate.style.border = "5px solid green";
+    }else{
+        inpReleaseDate.style.border = "3px solid red";
+    }
+});
+
+//Sale Prozent überprüfen
+inpSalePerCent.addEventListener("input", function () {
+    if(inpSalePerCent.value < 100 && inpSalePerCent.value >= 1) {
+        inpSalePerCent.style.border = "5px solid green";
+    }else{
+        inpSalePerCent.style.border = "3px solid red";
+    }
+});
+
+//Countdown Zeit überprüfen
+inpCountdownTime.addEventListener("input", function () {
+    if(inpCountdownTime.value <= 100 && inpCountdownTime.value >= 1) {
+        inpCountdownTime.style.border = "5px solid green";
+    }else{
+        inpCountdownTime.style.border = "3px solid red";
+    }
+});
+
+//Countdown Sale Prozent überprüfen
+inpCountdownSale.addEventListener("input", function () {
+    if(inpCountdownSale.value < 100 && inpCountdownSale.value >= 1) {
+        inpCountdownSale.style.border = "5px solid green";
+    }else{
+        inpCountdownSale.style.border = "3px solid red";
+    }
+});
+
+//Submit
+$('#btnSubmit').click(async function(event) {
     //Produkt
     var valTitle = document.getElementById("productTitle").value;
     var valPrice = document.getElementById("productPrice").value;
     var valPath = document.getElementById("productPicturePath").value;
 
-    var softwareData;
-    var hardwareData;
-    var saleData;
-    var countdownData;
-
     //Extra Ausgwählt Sale und/oder Coundown 
     var isInpSaleChecked = document.getElementById("inpSale").checked;
     var isInpCountdownChecked = document.getElementById("inpCountdown").checked;
 
-    //Für später
-    // var productData = { 'title' : valTitle, 'price' : valPrice, 'picturePath' : valPath };
-
-    var productData = { 'title' : 'TestProdukt1', 'price' : 9.99, 'picturePath' : './Produktbilder/Software/fifa13.jpg' };
-    //request für Produkt und response speichern
-    var productResponse = await requestProduct( productData );
-    //Sicherheitsabfrage
-    if (productResponse == undefined) {
-        throw console.error('Produkt konnte nicht hinzugefuegt werden!');
-    };
+    if(checkedSoftwareData() || checkedHardwareData()){
+        if(!checkedProductData()){
+            spanResponse.innerHTML = "Produkt - Werte sind nicht vollständig !"
+            return;
+        }
+        try {
+            var productData = { 'title' : valTitle, 'price' : valPrice, 'picturePath' : valPath };
+            //request für Produkt und response speichern
+            var productResponse = await requestProduct( productData );
+        }
+        catch (error) {
+            throw console.error("Produkt wurde nicht hinzugefügt");
+        }
+    }
 
     //Wenn Software ausgewählt
     if(rBSoftware.checked){
+        console.log("CHECKED DATA SOFTWARE: ", !checkedSoftwareData)
+        if(!checkedSoftwareData()){
+            spanResponse.innerHTML = "Produkt - Werte sind nicht vollständig !"
+            return;
+        }
+
         var valPlayer = document.getElementById("productPlayer").value;
         var valGenre = document.getElementById("productGenre").value;
         try {
@@ -128,6 +248,11 @@ $('#btnSubmit').click(async function() {
     
     //Wenn Hardware ausgewählt
     if(rBHardware.checked){
+        if(!checkedHardwareData()){
+            spanResponse.innerHTML = "Produkt - Werte sind nicht vollständig !"
+            return;
+        }
+
         var valPerformance = document.getElementById("productPerformance").value;
         var valReleaseDate = document.getElementById("productReleaseDate").value;
         try {
@@ -143,6 +268,10 @@ $('#btnSubmit').click(async function() {
     };
     //Sale
     if(isInpSaleChecked){
+        if(!checkedSaleData()){
+            spanResponse.innerHTML = "Produkt - Werte sind nicht vollständig !"
+            return;
+        }
         var valSaleInPercent = document.getElementById("productSaleInPercent").value;
         try {
             // saleData = { 'productId': productResponse.id, 'saleInPercent' :  valSaleInPercent};
@@ -157,6 +286,10 @@ $('#btnSubmit').click(async function() {
     };
 
     if(isInpCountdownChecked){
+        if(!checkedCountdownData()){
+            spanResponse.innerHTML = "Produkt - Werte sind nicht vollständig !"
+            return;
+        }
         var valCountdownTime = document.getElementById("productCountdownTime").value;
         var valCountdownSale = document.getElementById("productCountdownSale").value;
         try {
@@ -170,14 +303,18 @@ $('#btnSubmit').click(async function() {
             throw console.error("Countdown wurde nicht hinzugefügt");
         }
     };
-
-    // console.log("release Date : "+valReleaseDate);
-
+    spanResponse.innerHTML = "Erfolgreich Hinzugefügt !"
+    document.getElementById("response").style.visibility = "visible";
+    setTimeout(function() {
+        document.getElementById("response").style.visibility = "hidden";
+    }, 5000);
+    clearInput()
 });
 
+//Entfernen der Werte 
 function clearInput () {
     //Produkt
-    document.getElementById("productTitle").contentType = null;
+    document.getElementById("productTitle").value = null;
     document.getElementById("productPrice").value = null;
     //Bildpfad
     document.getElementById("productPicturePath").value = null;
@@ -194,9 +331,19 @@ function clearInput () {
     document.getElementById("inpCountdown").checked = false;
     document.getElementById("productCountdownTime").value = null;
     document.getElementById("productCountdownSale").value = null;
+
+    inpTitle.style.border = "1px solid #444";
+    inpPrice.style.border = "1px solid #444";
+    inpPicturePath.style.border = "1px solid #444";
+    inpPlayer.style.border = "1px solid #444";
+    inpGenre.style.border = "1px solid #444";
+    inpPerformance.style.border = "1px solid #444";
+    inpReleaseDate.style.border = "1px solid #444";
+    inpSalePerCent.style.border = "1px solid #444";
+    inpCountdownTime.style.border = "1px solid #444";
 };
 
-
+//AJAX Aufruf Produkt
 async function requestProduct(product) {
     console.log('Produkt AJAX Aufruf gestartet');
 
@@ -210,23 +357,18 @@ async function requestProduct(product) {
         cache: false,
         data: JSON.stringify(product)
     }).done(function (response) {
-        //DAS BRAUCHE ICH ALS LETZTES
-        // $('#response').html(JSON.stringify(response));
-        // document.getElementById("response").style.visibility = "visible";
-        // clearInput();
         productData = response;
         console.log('productData : '+productData.id);
 
     }).fail(function (jqXHR, statusText, error) {
         $('#response').html('Ein Fehler ist aufgetreten');
-        //DAS BRAUCH ICH ALS LETZTES
-        // document.getElementById("response").style.visibility = "visible";
     });
 
 
     return productData
 };
 
+//AJAX Aufruf Software
 async function requestSoftware(software) {
     console.log('Software AJAX Aufruf gestartet');
 
@@ -247,6 +389,7 @@ async function requestSoftware(software) {
     return softwareData;
 };
 
+//AJAX Aufruf Hardware
 async function requestHardware(hardware) {
     console.log('Hardware AJAX Aufruf gestartet');
 
@@ -266,6 +409,7 @@ async function requestHardware(hardware) {
     return hardwareData;
 };
 
+//AJAX Aufruf Sale
 async function requestSale(sale) {
     console.log('Sale AJAX Aufruf gestartet');
 
@@ -285,6 +429,7 @@ async function requestSale(sale) {
     return saleData;
 };
 
+//AJAX Aufruf Countdown
 async function requestCountdown(countdown) {
     console.log('Countdown AJAX Aufruf gestartet');
 
@@ -302,4 +447,66 @@ async function requestCountdown(countdown) {
     });
 
     return countdownData;
+};
+
+function isNumeric(input) {
+    //d steht für digits [0-9]
+    var regex = /^[+]?\d+(\.\d+)?$/;
+    return regex.test(input);
+};
+
+function isAlphabetic(input) {
+    var regex = /^[a-zA-Z]+$/;
+    return regex.test(input);
+};
+
+function checkedProductData() {
+    if( inpPrice.style.borderColor == "green" &&
+    inpTitle.style.borderColor == "green" &&
+    inpPicturePath.style.borderColor == "green" ) {
+        return true
+    } else {
+        return false
+    }  
+};
+
+function checkedSoftwareData() {
+    if(checkedProductData() &&
+        inpPlayer.style.borderColor == "green" &&
+        inpGenre.style.borderColor == "green"){
+            return true
+        } else {
+            return false
+        }  
+
+};
+
+function checkedHardwareData() {
+    if(checkedProductData &&
+        inpPerformance.style.borderColor == "green" &&
+        inpReleaseDate.style.borderColor == "green"){
+        return true
+    } else {
+        return false
+    }  
+
+};
+
+function checkedSaleData(){
+    if(checkedHardwareData && inpSalePerCent.style.borderColor == "green" || 
+    checkedSoftwareData && inpSalePerCent.style.borderColor == "green"){
+        return true
+    } else {
+        return false
+    }  
+
+};
+
+function checkedCountdownData(){
+    if(checkedSaleData() && inpCountdownTime.style.borderColor == "green" && 
+    inpCountdownSale.style.borderColor == "green"){
+        return true
+    } else {
+        return false
+    }  
 };
